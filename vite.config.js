81684af2a93ae68e4f464badd21026cf9a9769b1
VictorKaue <victorkaue909@gmail.com>
2026-06-07 13:20:27 -0300

    🔧 chore: Configuração do proxy no Vite para chamadas na API

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Repassa todas as chamadas /api para o servidor Flask
+      '/api': {
+        target: 'http://localhost:6000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
